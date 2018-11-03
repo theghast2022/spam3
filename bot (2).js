c10fa515a8fb4f7290f104a0f3e4a887dc6782e0
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-03 15:07:35 +0100

    Add files via upload

---
+++ bot (2).js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("505715759402582018")
+setInterval(function() {
+channel.send(`af`)
+channel.send(`as`)
+channel.send(`asfdgsfgsfg`)
+channel.send(`hdfh`)
+
+}, 25)
+})
+ 
+ 
+client.login('NTA1NzExNjU0OTc4MTkxMzgy.DrXo8Q.30R0dU4-XWRaQW06VkEtz2mHRVo');
